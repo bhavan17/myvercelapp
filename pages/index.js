9e2eea961b2fed3d9536be6409ede9c6a898b422
Bhavan P <bhavan.p9cbse@gmail.com>
2025-11-11 06:33:46 +0530

    index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '100px' }}>
+      <h1>Welcome to My Next.js App!</h1>
+      <p>This is deployed on Vercel!</p>
+    </div>
+  )
+}
